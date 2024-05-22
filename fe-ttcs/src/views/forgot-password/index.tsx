5a6c579386dbd0d2a49acb63408d2807c81be053
nguyenkien0703 <nguyenkien07032003ns@gmail.com>
2024-05-23 00:40:48 +0700

    add forgot password

--- fe-ttcs/src/views/forgot-password/index.tsx
+++ fe-ttcs/src/views/forgot-password/index.tsx
@@ -15,12 +15,12 @@ const ForgotPassword = () => {
     return (
         <AuthLayout>
             <div className="mb-10 flex items-center justify-center gap-5">
-                <Image
-                    src={'/images/logo-icon.png'}
-                    alt={''}
-                    width={48}
-                    height={48}
-                />
+                {/*<Image*/}
+                {/*    src={'/images/logo-icon.png'}*/}
+                {/*    alt={''}*/}
+                {/*    width={48}*/}
+                {/*    height={48}*/}
+                {/*/>*/}
                 <Text className="text-3xl font-bold">
                     {'SinatraNguyen Shop'}
                 </Text>
--- fe-ttcs/src/views/reset-password/index.tsx
+++ fe-ttcs/src/views/reset-password/index.tsx
@@ -69,12 +69,12 @@ const ResetPassword = () => {
         <>
             <AuthLayout>
                 <div className="mb-10 flex items-center justify-center gap-5">
-                    <Image
-                        src={'/images/logo-icon.png'}
-                        alt={''}
-                        width={48}
-                        height={48}
-                    />
+                    {/*<Image*/}
+                    {/*    src={'/images/logo-icon.png'}*/}
+                    {/*    alt={''}*/}
+                    {/*    width={48}*/}
+                    {/*    height={48}*/}
+                    {/*/>*/}
                     <Text className="text-3xl font-bold">
                         {'SinatraNguyen Shop'}
                     </Text>
